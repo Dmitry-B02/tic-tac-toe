cc876a8472e687a79c603f956130e5e6fa78e9d3
Dmitry-B02 <silaxer@mail.ru>
2020-03-05 21:19:55 +0300

    tic-tac-toe

--- src/main/java/main/java/Field.java
+++ src/main/java/main/java/Field.java
@@ -3,19 +3,15 @@ package main.java;
 import java.util.Arrays;
 import java.util.Objects;
 
-class Field {
+
+public class Field {
+
+    public enum line {
+        leftRight, upDown, mainDiagonal, secondaryDiagonal
+    }
+
     private int size;
     char[][] field;
-
-    Field() { // конструктор по умолчанию (3 клетки)
-        this.size = 3;
-        field = new char[this.size][this.size];
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                field[i][j] = ' ';
-            }
-        }
-    }
 
     Field(int size) { // конструктор для кастомного поля
         if (size < 3) throw new IllegalArgumentException("Wrong field size");
@@ -23,124 +19,126 @@ class Field {
         field = new char[this.size][this.size];
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                field[i][j] = ' ';
+                field[i][j] = '.';
             }
         }
     }
 
-    boolean inBounds(int vertical, int horizontal) {
+    public Field() { // конструктор по умолчанию (3 клетки)
+        this(3);
+    }
+
+    private boolean inBounds(int vertical, int horizontal) {
         return vertical < size && vertical >= 0 && horizontal < size && horizontal >= 0;
     }
 
-    private void add(int vertical, int horizontal, char element) { // метод добавления элемента
-        if (inBounds(vertical, horizontal) && field[vertical][horizontal] == ' ')
+    private boolean set(int vertical, int horizontal, char element) { // метод добавления элемента
+        if (inBounds(vertical, horizontal) && field[vertical][horizontal] == '.') {
             field[vertical][horizontal] = element;
-        else throw new IllegalArgumentException("Wrong input");
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    void addCross(int vertical, int horizontal) {
-        add(vertical, horizontal, '☦');
+    char get(int vertical, int horizontal) { // получение символа в заданной клетке
+        if (inBounds(vertical, horizontal)) return field[vertical][horizontal];
+        else throw new IllegalArgumentException("Wrong Input");
     }
 
-    void addCircle(int vertical, int horizontal) {
-        add(vertical, horizontal, '✡');
+    public boolean setCross(int vertical, int horizontal) {
+        return set(vertical, horizontal, '☦');
     }
 
-    void clear(int vertical, int horizontal) {
-        if (inBounds(vertical, horizontal)) {
-            if (field[vertical][horizontal] == ' ') {
-                throw new IllegalArgumentException("The cell is already empty");
+    public boolean setCircle(int vertical, int horizontal) {
+        return set(vertical, horizontal, '✡');
+    }
+
+    public boolean clear(int vertical, int horizontal) { // очистка указанной клетки
+        if (inBounds(vertical, horizontal) && field[vertical][horizontal] != '.') {
+            field[vertical][horizontal] = '.';
+            return true;
+        } else return false;
+    }
+
+    public void clear() { // очистка всего поля
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                clear(i, j);
             }
-            field[vertical][horizontal] = ' ';
-        } else throw new IllegalArgumentException("Wrong input");
+        }
     }
 
-    private int find(char element) {
+    private int checkDirection(line line, int vertical, int horizontal, char el, boolean direction) {
+        int currentY = vertical;
+        int currentX = horizontal;
+        int currentMax = 0;
+        while (inBounds(currentY, currentX) && field[currentY][currentX] == el) {
+            currentMax++;
+            switch(line) {
+                case leftRight:
+                    if (direction) {
+                        currentX++;
+                    }
+                    else {
+                        currentX--;
+                    }
+                    break;
+                case upDown:
+                    if (direction) {
+                        currentY++;
+                    }
+                    else {
+                        currentY--;
+                    }
+                    break;
+                case mainDiagonal:
+                    if (direction) {
+                        currentX++;
+                        currentY++;
+                    }
+                    else {
+                        currentX--;
+                        currentY--;
+                    }
+                    break;
+                case secondaryDiagonal:
+                    if (direction) {
+                        currentX++;
+                        currentY--;
+                    }
+                    else {
+                        currentX--;
+                        currentY++;
+                    }
+                    break;
+            }
+        }
+        return currentMax;
+    }
+
+    private int longestSequence(char el) {
         int max = 0;
-        for (int y = 0; y < size; y++) {
-            for (int x = 0; x < size; x++) {
-
-                // проверка влево-вправо
-                int maxH = 0;
-                if (field[y][x] == element) {
-                    int right = x + 1;
-                    int left = x - 1;
-                    while (inBounds(y, right) && field[y][right] == element) { // проверка вправо
-                        maxH++;
-                        right++;
-                    }
-                    while (inBounds(y, left) && field[y][left] == element) { // проверка влево
-                        maxH++;
-                        left--;
-                    }
-                    maxH++;
-                    if (maxH > max) max = maxH;
-
-                    // проверка вверх-вниз
-                    int maxV = 0;
-                    int up = y - 1;
-                    int down = y + 1;
-                    while (inBounds(up, x) && field[up][x] == element) { // проверка вверх
-                        maxV++;
-                        up--;
-                    }
-                    while (inBounds(down, x) && field[down][x] == element) { // проверка ввниз
-                        maxV++;
-                        down++;
-                    }
-                    maxV++;
-                    if (maxV > max) max = maxV;
-
-                    int maxD1 = 0;
-                    int maxD2 = 0;
-
-                    // проверка по 1 диагонали
-                    int upD = y - 1;
-                    int rightD = x + 1;
-                    int downD = y + 1;
-                    int leftD = x - 1;
-                    while (inBounds(upD, rightD) && field[upD][rightD] == element) { // проверка вправо-вверх
-                        maxD1++;
-                        upD--;
-                        rightD++;
-                    }
-                    while (inBounds(downD, leftD) && field[downD][leftD] == element) { // проверка влево-вниз
-                        maxD1++;
-                        downD++;
-                        leftD--;
-                    }
-                    maxD1++;
-                    if (maxD1 > max) max = maxD1;
-
-                    // проверка по 2 диагонали
-                    upD = y - 1;
-                    rightD = x + 1;
-                    downD = y + 1;
-                    leftD = x - 1;
-                    while (inBounds(upD, leftD) && field[upD][leftD] == element) { // проверка влево-вверх
-                        maxD2++;
-                        upD--;
-                        leftD--;
-                    }
-                    while (inBounds(downD, rightD) && field[downD][rightD] == element) { // проверка вправо-вниз
-                        maxD2++;
-                        downD++;
-                        rightD++;
-                    }
-                    maxD2++;
-                    if (maxD2 > max) max = maxD2;
+        int currentMax;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                for (line a : line.values()) {
+                    // вычитаю 1, т.к. в checkDirection считаю начальную клетку 2 раза
+                    currentMax = checkDirection(a, i, j, el, true) - 1;
+                    currentMax += checkDirection(a, i, j, el, false);
+                    if (max < currentMax) max = currentMax;
                 }
             }
         }
         return max;
     }
 
-    int longestCrossSequence() {
-        return find('☦');
+    public int longestCrossSequence() {
+        return longestSequence('☦');
     }
 
-    int longestCircleSequence() {
-        return find('✡');
+    public int longestCircleSequence() {
+        return longestSequence('✡');
     }
 
     @Override
@@ -150,5 +148,26 @@ class Field {
         return result;
     }
 
-    // TODO: override equals & toString
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                result.append(field[i][j]);
+                result.append(" ");
+            }
+            result.append('\n');
+        }
+        return result.toString();
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        else if (object instanceof Field) {
+            Field other = (Field) object;
+            return this.size == other.size &&
+                    Arrays.deepEquals(this.field, other.field);
+        } else return false;
+    }
 }
--- test/MainTest.java
+++ test/MainTest.java
@@ -1,5 +1,48 @@
+import org.junit.jupiter.api.Test;
+import main.java.Field;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class MainTest {
-    // TODO: TESTS
+class test { // тесты для крестиков эквивалентны тестам для ноликов, поэтому вторые опустил
+    Field testField = new Field();
+    @Test
+    void setSymbol() {
+        assertTrue(testField.setCross(1, 1));
+        assertFalse(testField.setCross(1, 1));
+        assertFalse(testField.setCross(0, 3));
+        assertFalse(testField.setCross(-1, -1));
+        assertFalse(testField.setCross('a', 'a'));
+    }
+    @Test
+    void clearSymbol() {
+        testField.setCross(1, 1);
+        assertTrue(testField.clear(1, 1));
+        assertFalse(testField.clear(1, 1));
+        assertFalse(testField.clear(3, 3));
+    }
+    @Test
+    void sequence() {
+        testField.clear();
+        testField.setCross(0, 0);
+        testField.setCross(0, 1);
+        testField.setCross(0, 2);
+        System.out.println(testField.toString());
+        testField.longestCrossSequence();
+        assertEquals(3, testField.longestCrossSequence());
+        testField.clear();
+        testField.setCross(0, 0);
+        testField.setCross(1, 1);
+        testField.setCross(2, 2);
+        assertEquals(3, testField.longestCrossSequence());
+        testField.clear();
+        testField.setCross(0, 2);
+        testField.setCross(1, 1);
+        testField.setCross(2, 0);
+        assertEquals(3, testField.longestCrossSequence());
+        testField.clear();
+        testField.setCross(0, 0);
+        testField.setCross(1, 0);
+        testField.setCross(2, 0);
+        assertEquals(3, testField.longestCrossSequence());
+    }
 }
